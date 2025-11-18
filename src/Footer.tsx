13677b423f6d8c22b2f559f316e03b0a5fefe57f
Николай Варюшин <pugsassts@gmail.com>
2025-11-18 13:08:12 +0300

    Divided the application into modules

---
+++ src/Footer.tsx
@@ -0,0 +1,20 @@
+export function Footer() {
+  return (
+    <footer className="page-footer  container">
+      <div className="page-footer__wrapper">
+        <ul className="page-footer__contacts  contacts">
+          <li>
+            <a className="contacts__link  contacts__link--twitter" href="#">
+              Twitter
+            </a>
+          </li>
+          <li>
+            <a className="contacts__link  contacts__link--vk" href="#">
+              VK
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
