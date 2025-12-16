0f40f241672d974ead9350074d2cf87606cf24cd
Николай Варюшин <pugsassts@gmail.com>
2025-12-16 18:05:09 +0300

    feat: Сделал логику, которая отвечает за выделение активной кнопки

--- src/components/ImageFilter.tsx
+++ src/components/ImageFilter.tsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
+
 export function ImageFilter() {
+  const idFilter = {
+    default: "default",
+    random: "random",
+    discussed: "discussed",
+  };
+  const [currentFilter, setCurrentFilter] = useState("default");
+
+  function changeFilter(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (!(e.target instanceof HTMLButtonElement)) return;
+
+    if (e.target.id !== currentFilter) {
+      setCurrentFilter(e.target.id);
+    }
+  }
+
   return (
     <section className="img-filters  img-filters--inactive  container">
       <h2 className="img-filters__title  visually-hidden">Фильтр фотографий</h2>
@@ -7,25 +25,33 @@ export function ImageFilter() {
         action="index.html"
         method="get"
         autoComplete="off"
+        onClick={(e) => changeFilter(e)}
       >
         <button
-          className="img-filters__button  img-filters__button--active"
+          className={`img-filters__button  ${
+            currentFilter === idFilter.default && "img-filters__button--active"
+          }`}
           type="button"
-          id="filter-default"
+          id={idFilter.default}
         >
           По умолчанию
         </button>
         <button
-          className="img-filters__button"
+          className={`img-filters__button  ${
+            currentFilter === idFilter.random && "img-filters__button--active"
+          }`}
           type="button"
-          id="filter-random"
+          id={idFilter.random}
         >
           Случайные
         </button>
         <button
-          className="img-filters__button"
+          className={`img-filters__button  ${
+            currentFilter === idFilter.discussed &&
+            "img-filters__button--active"
+          }`}
           type="button"
-          id="filter-discussed"
+          id={idFilter.discussed}
         >
           Обсуждаемые
         </button>
